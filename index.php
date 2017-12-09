370c5d809301887a5ba98e79550c17643779f827
cakyus <cakyus@gmail.com>
2017-12-09 17:04:46 +0700

    * use ROOT_DIR

--- index.php
+++ index.php
@@ -3,14 +3,11 @@
 spl_autoload_register(function($className){
 	$fileName = $className;
 	$fileName = str_replace('\\', '/', $fileName);
-	$fileName = 'src/'.$fileName.'.php';
+	$fileName = dirname(__FILE__).'/src/'.$fileName.'.php';
 	if (is_file($fileName)) {
 		require_once($fileName);
 	}
 });
-
-// require_once('src/Application.php');
-// require_once('src/Controller/Index.php');
 
 $application = new \Application;
 $application->start();
