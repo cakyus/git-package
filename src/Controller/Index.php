5bdc6e7ca04ac97361a1aea36f343d6ecfc315c9
cakyus <cakyus@gmail.com>
2017-12-19 23:29:18 +0700

    + create project

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -51,6 +51,67 @@ class Index {
 		}
 	}
 
+	// DOC   create <vendorName/projectName> - Create new project
+
+	public function create($packageName) {
+
+		if (!preg_match("/^([a-z][a-z0-9]*)\/([a-z][a-z0-9]*)$/", $packageName, $match)) {
+			throw new \Exception("Invalid projectName");
+		}
+
+		$vendorName = $match[1];
+		$projectName = $match[2];
+
+		$projectDir = tempnam(sys_get_temp_dir(), 'php');
+
+		unlink($projectDir);
+		mkdir($projectDir);
+
+		echo "create package $vendorName/$projectName in $projectDir ..\n";
+
+		echo "create LICENSE ..\n";
+		copy('LICENSE', $projectDir.'/LICENSE');
+
+		echo "create bin/ ..\n";
+		mkdir($projectDir.'/bin');
+		touch($projectDir.'/bin/.htaccess');
+
+		echo "create docs/ ..\n";
+		mkdir($projectDir.'/docs');
+		touch($projectDir.'/docs/.htaccess');
+
+		echo "create src/ ..\n";
+		mkdir($projectDir.'/src');
+		touch($projectDir.'/src/.htaccess');
+
+		echo "create tests/ ..\n";
+		mkdir($projectDir.'/tests');
+		touch($projectDir.'/tests/.htaccess');
+
+		echo "create gitignore\n";
+		file_put_contents($projectDir.'/.gitignore', 'vendor');
+
+		echo "create composer.json ..\n";
+		$text = '{
+	 "name": "'.$packageName.'"
+	,"description": "'.$packageName.'"
+	,"license": "GPL-2.0"
+	,"autoload": {
+		"psr-4": {
+			 "\\\\'.ucwords($vendorName).'\\\\'.ucwords($projectName).'\\\\": "src/"
+			,"\\\\'.ucwords($vendorName).'\\\\'.ucwords($projectName).'\\\\Test\\\\": "tests/"
+		}
+	}
+}';
+		file_put_contents($projectDir.'/composer.json', $text);
+	}
+
+	// DOC   list - List repositories
+
+	public function list() {
+
+	}
+
 	// DOC   help - Show this information and exit
 
 	public function help() {
